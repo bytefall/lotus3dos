877c4470effa0251a0d9e0417a0c9b906bb1b9a3
Oleg Scherbakov <scherbakov.oleg@gmail.com>
2020-05-09 21:37:30 +0300

    Make it compile.

--- src/main.asm
+++ src/main.asm
@@ -6,22 +6,52 @@
 segment main use16
 
 start:
-	; cs:5178
+	; bp cs:060b
 
-	call	dos:set_video_mode_and_timer
-	call	arc:open_and_read_data_file
+	; cs:5178
+	; mov		ss, [cs:word_5164]
+	; mov		sp, 1800h
+
+	; mov		[cs:word_5160], ds
+	; mov		ds, [cs:word_5162]
+
+	mov		ax, text
+	mov		ds, ax
+
+	; cs:518A
+	call	set_video_mode_and_timer
+	call	open_and_read_data_file
 
 	mov		ax, 4c00h
 	int		21h
 
+word_5160:			dw ?
 word_5162:			dw ?									; cs:5162 - temporary storage for data segment (0F6C)
 word_5164:			dw ?
+word_5166:			dw ?
+word_5168:			dw ?
+word_516A:			dw ?
+counter:			db ?
+byte_516D:			db ?
+word_516E:			dw ?
+byte_5170:			db ?
+					db 90h
+idx:				dw ?
+word_5174:			dw ?
+word_5176:			dw ?
+
+include 'sound.inc'
+include 'game.inc'
+include 'archive.inc'
+include 'video.inc'
+include 'timer.inc'
+include 'dos.inc'
 
 segment text
 
-arc_file_name:		db 'LOTUS.DAT', 0
-arc_file_handle:	dw ?
-arc_header:			times 2816 db 0
+arc_header:			times 2816 db 0							; 3164
+arc_file_name:		db 'LOTUS.DAT', 0						; 3C64
+arc_file_handle:	dw ?									; 3C6E
 
 create_error:		db 'ERROR: Can''t create $', 0			; 3E34
 open_error:			db 'ERROR: Can''t open $', 0			; 3E4A
@@ -63,12 +93,16 @@ word_3D7A:			dw ?
 byte_3D7C:			db ?
 word_3D7E:			dw ?
 word_3D80:			dw ?
+word_3D82:			dw ?
+word_3D84:			dw ?
 byte_3D86:			db ?
 byte_3D87:			db ?
 arr_3D88:			times 64 db 0							; 3D88: 64? maybe need less
 byte_3DD0:			db ?
 byte_3DD8:			db ?
 word_3E08:			dw ?
+word_3E0A:			dw ?
+word_3E0C:			dw ?
 byte_3E0E:			db ?
 byte_3E0F:			db ?
 byte_3E10:			db ?
@@ -105,9 +139,3 @@ arr_DBB4:			times 14 db 0							; DBB4
 
 word_20:			dw ?									; es:0020
 tmp_cs:				dw ?									; es:0022
-
-segment arc
-	include 'archive.inc'
-
-segment dos
-	include 'dos.inc'
